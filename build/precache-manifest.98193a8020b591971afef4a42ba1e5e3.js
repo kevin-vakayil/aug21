573f930f24464901e759cb6512301959399caf22
kevin-vakayil <87632641+kevin-vakayil@users.noreply.github.com>
2021-08-21 10:18:46 +0530

    Add files via upload

---
+++ build/precache-manifest.98193a8020b591971afef4a42ba1e5e3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42fb530f5e1d72eb6670cac04261ef4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f9c96d18be438a668b9",
+    "url": "/static/css/2.4efcbf08.chunk.css"
+  },
+  {
+    "revision": "1f2738f0f175c44dc6a0",
+    "url": "/static/css/main.18e107e6.chunk.css"
+  },
+  {
+    "revision": "3f9c96d18be438a668b9",
+    "url": "/static/js/2.1c347ae1.chunk.js"
+  },
+  {
+    "revision": "1f2738f0f175c44dc6a0",
+    "url": "/static/js/main.19672b3c.chunk.js"
+  },
+  {
+    "revision": "d373dbff63881bc61533",
+    "url": "/static/js/runtime-main.e863456c.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
